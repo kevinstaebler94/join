8914a753e99aee3aabcc568305e3cda7bb5b509a
Daniel Bumbuc <bumbucd@gmail.com>
2025-03-11 23:49:27 +0100

    dropdown menu edited

---
+++ scripts/add_task.js
@@ -0,0 +1,13 @@
+function toggleDropdownName() {
+    let dropdown = document.getElementById('dropdownListName');
+    let dropdownIcon = document.getElementById('dropdownIconName');
+    dropdown.classList.toggle('dNone');
+    dropdownIcon.classList.toggle('rotate');
+}
+
+function toggleDropdownCategory() {
+    let dropdown = document.getElementById('dropdownListCategory');
+    let dropdownIcon = document.getElementById('dropdownIconCategory');
+    dropdown.classList.toggle('dNone');
+    dropdownIcon.classList.toggle('rotate');
+}
