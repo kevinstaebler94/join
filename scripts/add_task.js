b69755c38ece34e96f55bd1df100a6ed4fa1192b
Daniel Bumbuc <bumbucd@gmail.com>
2025-03-13 23:04:25 +0100

    priority button and input border edited

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -1,15 +1,35 @@
 function toggleDropdownName() {
+    let customDropdownName = document.getElementById('customDropdownName');
     let dropdown = document.getElementById('dropdownListName');
     let dropdownIcon = document.getElementById('dropdownIconName');
+    customDropdownName.classList.toggle('activeBorder');
+    // dropdown.classList.toggle('dNone');
+    // dropdown.classList.toggle('transformList');
+    dropdownIcon.classList.toggle('rotate');
+    toggleAnimation(dropdown);
+}
+
+function toggleDropdownCategory() {
+    let customDropdownCategory = document.getElementById('customDropdownCategory');
+    let dropdown = document.getElementById('dropdownListCategory');
+    let dropdownIcon = document.getElementById('dropdownIconCategory');
+    customDropdownCategory.classList.toggle('activeBorder');
     dropdown.classList.toggle('dNone');
     dropdownIcon.classList.toggle('rotate');
 }
 
-function toggleDropdownCategory() {
-    let dropdown = document.getElementById('dropdownListCategory');
-    let dropdownIcon = document.getElementById('dropdownIconCategory');
-    dropdown.classList.toggle('dNone');
-    dropdownIcon.classList.toggle('rotate');
+function toggleAnimation(dropdown) {
+    
+
+    if (dropdown.classList.contains("visible")) {
+        dropdown.classList.remove("visible");
+        setTimeout(() => {
+            dropdown.classList.add("hidden");
+        }, 300); // Timeout auf die Transition-Dauer setzen
+    } else {
+        dropdown.classList.remove("hidden");
+        dropdown.classList.add("visible");
+    }
 }
 
 function selectPriority(priority) {
@@ -19,27 +39,33 @@ function selectPriority(priority) {
     let urgentIcon = document.getElementById('urgentIcon');
     let mediumIcon = document.getElementById('mediumIcon');
     let lowIcon = document.getElementById('lowIcon');
-    removePriorityClassList(urgentBtn, mediumBtn, lowBtn, urgentIcon, mediumIcon, lowIcon);
-    addPriorityClassList(priority, urgentBtn, mediumBtn, lowBtn, urgentIcon, mediumIcon, lowIcon);
+    resetPriorityClassList(urgentBtn, mediumBtn, lowBtn, urgentIcon, mediumIcon, lowIcon);
+    setPriorityClassList(priority, urgentBtn, mediumBtn, lowBtn, urgentIcon, mediumIcon, lowIcon);
 }
 
-function removePriorityClassList(urgentBtn, mediumBtn, lowBtn, urgentIcon, mediumIcon, lowIcon) {
+function resetPriorityClassList(urgentBtn, mediumBtn, lowBtn, urgentIcon, mediumIcon, lowIcon) {
+    urgentBtn.classList.add('hoverBtn');
     urgentBtn.classList.remove('activeUrgentBtn');
     urgentIcon.classList.remove('whiteIcon');
+    mediumBtn.classList.add('hoverBtn');
     mediumBtn.classList.remove('activeMediumBtn');
     mediumIcon.classList.remove('whiteIcon');
+    lowBtn.classList.add('hoverBtn');
     lowBtn.classList.remove('activeLowBtn');
     lowIcon.classList.remove('whiteIcon');
 }
 
-function addPriorityClassList(priority, urgentBtn, mediumBtn, lowBtn, urgentIcon, mediumIcon, lowIcon) {
+function setPriorityClassList(priority, urgentBtn, mediumBtn, lowBtn, urgentIcon, mediumIcon, lowIcon) {
     if (priority === 'urgent') {
+        urgentBtn.classList.remove('hoverBtn');
         urgentBtn.classList.add('activeUrgentBtn');
         urgentIcon.classList.add('whiteIcon');
     } else if (priority === 'medium') {
+        mediumBtn.classList.remove('hoverBtn');
         mediumBtn.classList.add('activeMediumBtn');
         mediumIcon.classList.add('whiteIcon');
     } else if (priority === 'low') {
+        lowBtn.classList.remove('hoverBtn');
         lowBtn.classList.add('activeLowBtn');
         lowIcon.classList.add('whiteIcon');
     }
