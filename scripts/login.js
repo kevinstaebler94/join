136bff32437f0292c77d2f463754f8cbc2bf5e1a
Kevin <kevinstaebler1994@outlook.de>
2025-03-26 22:24:10 +0100

    added formvalidation to login html

--- scripts/login.js
+++ scripts/login.js
@@ -65,19 +65,19 @@ function loginTemplate() {
       <div class="loginContainer">
         <h2>Log in</h2>
         <hr class="hr">
-        <form onsubmit="login(); return false">
+        <form onsubmit="login(event); return false">
           <div class="inputWrapper">
-            <input id="emailLogin" class="inputfield" type="email" placeholder="Email">
+            <input id="emailLogin" class="inputfield" type="email" placeholder="Email" oninput="toggleLoginButton()">
             <img class="inputIcon" src="./assets/img/mail.svg" alt="">
             <span class="hide">Placeholder</span>
           </div>
           <div class="inputWrapper">
-            <input id="passwordLogin" class="inputfield" type="password" placeholder="Password">
+            <input id="passwordLogin" class="inputfield" type="password" placeholder="Password" oninput="toggleLoginButton()">
             <img id="passwordIcon" class="inputIcon password" src="./assets/img/lock.svg" alt="">
-            <span class="hide">Placeholder</span>
+            <span id="errorMsgLogin" class="errorMsgPassword hide">Check your email and password. Please try again.</span>
           </div>
           <div class="buttonWrapper">
-            <button disabled type="button" id="loginButton" class="button loginButton">Log in</button>
+            <button disabled  type="submit" id="loginButton" class="button loginButton">Log in</button>
             <a class="button guestLoginButton" href="summary.html">Guest Log in</a>
           </div>
         </form>
@@ -90,11 +90,62 @@ function loginTemplate() {
   `
 }
  
+function login(event) {
+  event.preventDefault();
+  verifyLogin();
+}
 
+function verifyLogin() {
+  let email = document.getElementById("emailLogin").value.trim();
+  let password = document.getElementById("passwordLogin").value.trim();
+  let users = globalBackend.flatMap(obj => obj.users);
+  let user = users.find(user => user.email === email && user.password === password);
+  let error = document.getElementById("errorMsgLogin");
 
+  if(user) {
+    hideLoginErrorMessage(error)
+    window.location.href = "summary.html";
+  } else {
+    document.getElementById("passwordLogin").value = "";
+    showLoginErrorMessage(error);
+    setTimeout(function() {
+      hideLoginErrorMessage(error)
+    }, 3500)
+  }
+  return;
+}
 
+function toggleLoginButton() {
+  let email = document.getElementById("emailLogin").value.trim();
+  let pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  let password = document.getElementById("passwordLogin").value.trim();
+  let loginButton = document.getElementById("loginButton");
+  
+  if(pattern.test(email)) {
+    if(email.length > 0 && password.length > 3) {
+      loginButton.disabled = false;
+    } else {
+      loginButton.disabled = true;
+    }
+  }
+}
 
- 
+function showLoginErrorMessage(error) {
+    error.textContent = "Check your email and password. Please try again.";
+    error.classList.remove("hide");
+    document.getElementById("emailLogin").classList.add("redBorder");
+    document.getElementById("passwordLogin").classList.add("redBorder");
+    return false;
+}
+
+function hideLoginErrorMessage(error) {
+  error.classList.add("hide");
+  error.classList.remove("show");
+  document.getElementById("emailLogin").classList.remove("redBorder");
+  document.getElementById("passwordLogin").classList.remove("redBorder");
+  return true;
+}
+
 
 
 
