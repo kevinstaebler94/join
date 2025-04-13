ce2e7c6c0fd177abd9c28ae03d44e261248f7d77
Oliver Geschine <oli.geschine@web.de>
2025-04-13 15:12:51 +0200

    update contacts formvalidation

--- scripts/contactsModal.js
+++ scripts/contactsModal.js
@@ -38,16 +38,19 @@ function getContactsModalStructure() {
                     <div class="inputFieldWrapper">
                         <input class="inputFields" type="text" placeholder="Name" id="contactName" required>
                         <img class="inputIcon" src="./assets/img/person.svg">
+                        <span id="namePlaceholderError" class="errorMsg">Placeholder</span>
                     </div>
                     <div id="nameError" class="inputError dNone">Fehlertext</div>
                     <div class="inputFieldWrapper">
                         <input class="inputFields" type="email" placeholder="Email" id="contactEmail" required>
                         <img class="inputIcon" src="./assets/img/mail.svg">
+                        <span id="emailPlaceholderError" class="errorMsg">Placeholder</span>
                     </div>
                     <div id="emailError" class="inputError dNone">Fehlertext</div>
                     <div class="inputFieldWrapper">
                         <input class="inputFields" type="text" placeholder="Phone" id="contactPhone" required>
                         <img class="inputIcon" src="./assets/img/phone.svg">
+                        <span id="phonePlaceholderError" class="errorMsg">Placeholder</span>
                     </div>
                     <div id="phoneError" class="inputError dNone">Fehlertext</div>
                 </div>
@@ -129,6 +132,12 @@ function resetContactInputErrors(inputs) {
         el.classList.add('dNone');
         el.innerText = "";
     });
+
+    ['namePlaceholderError', 'emailPlaceholderError', 'phonePlaceholderError'].forEach(id => {
+        let el = document.getElementById(id);
+        el.classList.remove('visible');
+        el.innerText = "Placeholder";
+    });
 }
 
 function checkEmptyFields(inputs, values) {
@@ -136,22 +145,25 @@ function checkEmptyFields(inputs, values) {
 
     if (!values.name) {
         inputs.nameInput.classList.add('error');
-        inputs.nameError.innerText = "Please enter a name.";
-        inputs.nameError.classList.remove('dNone');
+        document.getElementById('namePlaceholderError').innerHTML = "Please enter a name.";
+        document.getElementById('namePlaceholderError').classList.add('visible');
+        hideErrorMessages('namePlaceholderError', 'contactName');
         hasError = true;
     }
 
     if (!values.email) {
         inputs.emailInput.classList.add('error');
-        inputs.emailError.innerText = "Please enter an e-mail adress.";
-        inputs.emailError.classList.remove('dNone');
+        document.getElementById('emailPlaceholderError').innerHTML = "Please enter an e-mail adress.";
+        document.getElementById('emailPlaceholderError').classList.add('visible');
+        hideErrorMessages('emailPlaceholderError', 'contactEmail');
         hasError = true;
     }
 
     if (!values.phone) {
         inputs.phoneInput.classList.add('error');
-        inputs.phoneError.innerText = "please enter a phone number.";
-        inputs.phoneError.classList.remove('dNone');
+        document.getElementById('phonePlaceholderError').innerHTML = "Please enter a phone number.";
+        document.getElementById('phonePlaceholderError').classList.add('visible');
+        hideErrorMessages('phonePlaceholderError', 'contactPhone');
         hasError = true;
     }
 
@@ -166,25 +178,42 @@ function checkDuplicateFields(inputs, values, existingContacts) {
 
         if (contact.name?.trim().toLowerCase() === values.name) {
             inputs.nameInput.classList.add('error');
-            inputs.nameError.innerText = "Name already used.";
-            inputs.nameError.classList.remove('dNone');
+            document.getElementById('namePlaceholderError').innerHTML = "Name already used.";
+            document.getElementById('namePlaceholderError').classList.add('visible');
+            hideErrorMessages('namePlaceholderError', 'contactName');
             hasError = true;
         }
 
         if (contact.email?.trim().toLowerCase() === values.email) {
             inputs.emailInput.classList.add('error');
-            inputs.emailError.innerText = "E-Mail already used.";
-            inputs.emailError.classList.remove('dNone');
+            document.getElementById('emailPlaceholderError').innerHTML = "E-Mail already used.";
+            document.getElementById('emailPlaceholderError').classList.add('visible');
+            hideErrorMessages('emailPlaceholderError', 'contactEmail');
             hasError = true;
         }
 
         if (contact.phone?.trim() === values.phone) {
             inputs.phoneInput.classList.add('error');
-            inputs.phoneError.innerText = "Phone number already used.";
-            inputs.phoneError.classList.remove('dNone');
+            document.getElementById('phonePlaceholderError').innerHTML = "Phone number already used.";
+            document.getElementById('phonePlaceholderError').classList.add('visible');
+            hideErrorMessages('phonePlaceholderError', 'contactPhone');
             hasError = true;
         }
     }
 
     return hasError;
+}
+
+function hideErrorMessages(id, inputId) {
+    setTimeout(() => {
+        let el = document.getElementById(id);
+        let input = document.getElementById(inputId);
+        if (el) {
+            el.classList.remove('visible');
+            el.innerText = "Placeholder";
+        }
+        if (input) {
+            input.classList.remove('error');
+        }
+    }, 3000);
 }
