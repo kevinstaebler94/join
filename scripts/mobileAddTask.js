4e5c19b6fd8a44d70e1c01981bcf8b1872555ebb
Daniel Bumbuc <bumbucd@gmail.com>
2025-05-13 21:11:24 +0200

    add responsive design for addTask.html

---
+++ scripts/mobileAddTask.js
@@ -0,0 +1,38 @@
+function checkWindowSize() {
+    if (window.innerWidth <= 850) {
+        let submitSection = document.getElementById('submitSection');
+        let mobileInfo = document.getElementById('mobileInfo');
+        submitSection.innerHTML = `<div class="submitBtnContainer">
+                                <span class="submitBtn clearBtn hoverBtn" onclick="clearAddTask()">Clear
+                                    <img class="submitIcons" src="./assets/img/cancelIcon.svg" alt="">
+                                    <img class="submitIcons" src="./assets/img/cancelIconHover.svg" alt="">
+                                </span>
+                                <span class="submitBtn checkBtn hoverBtn" onclick="checkValidation()">Create Task
+                                    <img class="submitIcons" src="./assets/img/createIcon.svg" alt="">
+                                </span>
+                            </div>`;
+        mobileInfo.innerHTML = `<div class="infoText">
+                                <p>
+                                <p class="fontRed">*</p>This field is required</p>
+                            </div>`;
+    } else {
+        submitSection.innerHTML = `<div class="infoText">
+                                <p>
+                                <p class="fontRed">*</p>This field is required</p>
+                            </div>
+
+                            <div class="submitBtnContainer">
+                                <span class="submitBtn clearBtn hoverBtn" onclick="clearAddTask()">Clear
+                                    <img class="submitIcons" src="./assets/img/cancelIcon.svg" alt="">
+                                    <img class="submitIcons" src="./assets/img/cancelIconHover.svg" alt="">
+                                </span>
+                                <span class="submitBtn checkBtn hoverBtn" onclick="checkValidation()">Create Task
+                                    <img class="submitIcons" src="./assets/img/createIcon.svg" alt="">
+                                </span>
+                            </div>`;
+        mobileInfo.innerHTML = '';
+    }
+}
+
+window.addEventListener('DOMContentLoaded', checkWindowSize);
+window.addEventListener('resize', checkWindowSize);
