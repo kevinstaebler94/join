bbc1e074f1544386ea277b0790d58112a34ddd22
Daniel Bumbuc <bumbucd@gmail.com>
2025-06-04 01:02:38 +0200

    fix formvalidation show all required fields + remove required oninput + add calender dateInput + checkDateValidation

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -63,6 +63,8 @@ function toggleDropdownName() {
     dropdownIcon.classList.toggle('rotate');
     listContainer.classList.toggle('dNone');
     checkIsOpen(assignedInput);
+
+
 }
 
 /**
@@ -228,18 +230,58 @@ function checkValidation() {
     let dateInput = document.getElementById('dateInput');
     let requiredDate = document.getElementById('requiredDate');
     let selectedCategory = document.getElementById('selectedCategory');
+    let categoryInput = document.getElementById('customDropdownCategory');
+    let requiredCategory = document.getElementById('requiredCategory');
     if (titleInput.value == '') {
         titleInput.classList.add('required');
         requiredTitle.innerHTML = `<p class="fontRed requiredFont">This field is required</p>`;
-    } else if (dateInput.value.length < 10) {
+    } if (dateInput.value.length < 10) {
         dateInput.classList.add('required');
         requiredDate.innerHTML = `<p class="fontRed requiredFont">This field is required</p>`;
-    } else if (selectedCategory.innerHTML == 'Select category') {
-        titleInput.classList.add('required');
+    } if (selectedCategory.innerHTML == 'Select category') {
+        categoryInput.classList.add('required');
+        requiredCategory.innerHTML = `<p class="fontRed requiredFont">This field is required</p>`;
     } else {
         pushTasks(loggedInUser, assignedArr);
         showAddedBoardImg();
     }
+}
+
+function checkDateValidation() {
+    setTimeout(() => {
+        let dateInput = document.getElementById('dateInput');
+        let requiredDate = document.getElementById('requiredDate');
+        if (dateInput.value.length < 10) {
+            dateInput.classList.add('required');
+            // requiredDate.classList.remove('requiredDate');
+            requiredDate.innerHTML = `<p class="fontRed requiredFont">This field is required</p>`;
+        }
+    }, 10);
+}
+
+function removeTitleRequired() {
+    let titleInput = document.getElementById('titleInput');
+    let requiredTitle = document.getElementById('requiredTitle');
+    if (titleInput.value.length > 0) {
+        titleInput.classList.remove('required');
+        requiredTitle.innerHTML = '';
+    }
+}
+
+function removeDateRequired() {
+    let dateInput = document.getElementById('dateInput');
+    let requiredDate = document.getElementById('requiredDate');
+    if (dateInput.value.length > 0) {
+        dateInput.classList.remove('required');
+        requiredDate.innerHTML = '';
+    }
+}
+
+function removeCategoryRequired() {
+    let categoryInput = document.getElementById('customDropdownCategory');
+    let requiredCategory = document.getElementById('requiredCategory');
+    categoryInput.classList.remove('required');
+    requiredCategory.innerHTML = '';
 }
 
 /**
@@ -481,4 +523,22 @@ window.addEventListener('mousedown', function (event) {
             styleDropdown(contactListContainer, assignedInput);
         }
     }, 30);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    flatpickr("#calendarWrap", {
+        wrap: true,
+        dateFormat: "d/m/Y",
+        allowInput: true,
+        minDate: "today",
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    flatpickr("#calendarWrap", {
+        wrap: true,
+        dateFormat: "d/m/Y",
+        allowInput: true,
+        minDate: "today",
+    });
 });
